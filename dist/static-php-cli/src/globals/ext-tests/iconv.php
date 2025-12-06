9bd9992922d7a321d5ef36dbfaf62ad54fb9f502
Rehmat Alam <contact@rehmat.works>
2025-12-06 14:18:24 +0500

    Bump libxml2 to 2.15.1 for static build

--- dist/static-php-cli/src/globals/ext-tests/iconv.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-assert(function_exists('iconv'));
-assert(iconv('UTF-8', 'CP437', 'foo') === 'foo');
--- dist/static-php-cli/src/globals/ext-tests/msgpack.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-assert(function_exists('msgpack_pack'));
-assert(function_exists('msgpack_unpack'));
-assert(msgpack_unpack(msgpack_pack(['foo', 'bar'])) === ['foo', 'bar']);
